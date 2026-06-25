403cfc938f139edd880c31f11ace78ef7577600b
Subathra-subu <subathrasubu2004@gmail.com>
2026-06-25 22:31:36 +0530

    goto command updated

--- tests/PageObjectCommands_login.spec.ts
+++ tests/PageObjectCommands_login.spec.ts
@@ -1,6 +1,8 @@
 import {test,expect } from '@playwright/test';
 test('Valid Login',async({page})=>{
-   await page.goto('https://automationexercise.com/');
+   await page.goto('https://automationexercise.com/',{
+     waitUntil: "domcontentloaded"
+   });
    await page.locator("//a[normalize-space()='Signup / Login']").click();
    await page.locator('//input[@data-qa="login-email"]').fill('emilydavis@gmail.com');
    await page.locator('//input[@data-qa="login-password"]').fill('12345');
