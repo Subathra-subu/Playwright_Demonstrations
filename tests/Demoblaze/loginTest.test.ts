8929a4feefa5d1ca015344558dcea11e152f4c01
Subathra-subu <subathrasubu2004@gmail.com>
2026-06-30 17:10:24 +0530

    DemoBlazeLoginTest added

---
+++ tests/Demoblaze/loginTest.test.ts
@@ -0,0 +1,41 @@
+import { expect,test } from "@playwright/test";
+import login_data from "../../testData/login_data.json" with {type:"json"};
+
+interface LoginData{
+    testName:string,
+    userName:string,
+    password:string,
+    expectedResult:"Success"|"Failure",
+    expectedMessage:string;
+}
+
+const users = login_data as LoginData[];
+
+for(const data of users){
+
+    console.log(data);
+
+    test(`${data.testName}-${data.userName || "Blank Username"}`,async({page})=>{
+        await page.goto("https://www.demoblaze.com/");
+        await page.locator("#login2").click();
+        await page.locator("#loginusername").fill(`${data.userName}`);
+        await page.locator("#loginpassword").fill(`${data.password}`);
+       
+        page.on("dialog",async (alert)=>{
+        await alert.accept();
+        })
+        
+        await page.locator("//button[text()='Log in']").click({timeout:40000});
+
+        if(data.expectedResult==="Success"){
+            await page.waitForTimeout(2000);
+        
+
+        await expect(page.locator("#nameofuser")).toBeVisible({timeout:10000});
+
+        await expect(page.locator('#nameofuser')).toContainText(`Welcome ${data.userName}`);
+        }
+        
+    });
+}
+
