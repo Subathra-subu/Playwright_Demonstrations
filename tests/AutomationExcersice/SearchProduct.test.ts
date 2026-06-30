bf4859b5fb087940df7d6a29de99b66f6ccd8750
Subathra-subu <subathrasubu2004@gmail.com>
2026-06-30 08:28:49 +0530

    Updated

--- tests/AutomationExcersice/SearchProduct.test.ts
+++ tests/AutomationExcersice/SearchProduct.test.ts
@@ -1,6 +1,28 @@
 import {test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+
+    await page.route('**/*', route => {
+        const url = route.request().url();
+
+        if (
+            url.includes('googleads') ||
+            url.includes('doubleclick') ||
+            url.includes('googlesyndication') ||
+            url.includes('adservice')
+        ) {
+            route.abort();
+        } else {
+            route.continue();
+        }
+    });
+
+    await  page.goto('https://automationexercise.com');
+
+});
+
 test('Search product',async({page})=>{
-  await  page.goto('https://automationexercise.com');
+  
   await expect(page.locator("//a[normalize-space()='Home']")).toBeVisible();
   await page.click('//a[text()=" Products"]');
   await expect(page).toHaveTitle('Automation Exercise - All Products');
